9586b416087e6723f9247e00ddc73c8e6ff0aac1
kerleau <37364328+kerleau@users.noreply.github.com>
2023-03-07 13:07:23 +0000

    add print to IT test

--- src/test/java/guru/springframework/JavaHelloWorldIT.java
+++ src/test/java/guru/springframework/JavaHelloWorldIT.java
@@ -10,6 +10,6 @@ public class JavaHelloWorldIT {
     @Test
     void myFauxIntegrationTest() {
 
-        System.out.println("My IT Ran");
+        System.out.println("My IT Ran ..... 777");
     }
 }
